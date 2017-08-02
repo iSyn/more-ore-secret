b9dc799dfc516ae910c193a14748f36541376c15
Synclair Wang <synclairwang11@yahoo.com>
2017-08-02 15:23:22 -0400

    added a different zone and can harvest wood

--- v0.2/main.js
+++ v0.2/main.js
@@ -6,12 +6,12 @@ let Game = {}
 Game.Launch = () => {
   console.log('Game loaded and launched')
 
-  Game.ores = 0
+  Game.ores = 40
   Game.refined = 0
   Game.wood = 0
 
   Game.oresPerClick = 1
-  Game.woodPerClick = 0
+  Game.woodPerClick = 1
 
   Game.selectedZone = 'mine'
   Game.priceIncrease = 1.15
@@ -32,11 +32,23 @@ Game.Launch = () => {
 
   s('#ore').onclick = Game.oreClick
 
+  Game.woodClick = () => {
+    Game.calculateClick()
+    Game.earn(Game.woodPerClick, 'wood')
+    Game.rebuildInventory()
+  }
+
+  s('#wood').onclick = Game.woodClick
+
   Game.calculateClick = () => {
-    let amount = 0
-    amount += Game.oresPerClicks
+    let oreAmount = 0
+    let woodAmount = 0
+    oreAmount += Game.oresPerClick
+    woodAmount += Game.woodPerClick
+
     // calculate the upgrdes and update Game.oresPerClick
-    Game.oresPerClicks = amount
+    Game.oresPerClick = oreAmount
+    Game.woodPerClick = woodAmount
   }
 
   Game.rebuildInventory = () => {
@@ -63,8 +75,8 @@ Game.Launch = () => {
     this.changeText = (number) => {
       s(`#store-button${number}`).innerHTML = `
         <div style='height: 100%; width: 100%' onclick='Game.items[${number}].buy()'>
-          <h1>${this.name} <span style='font-size: 15px'>[owned: ${this.owned}]</span> <span style='font-size: 25px; float: right'>${this.price} ${this.priceMaterial}</span></h1>
-          <hr>
+          <h1 onclick='Game.items[${number}].buy()'>${this.name} <span style='font-size: 15px'>[owned: ${this.owned}]</span> <span style='font-size: 25px; float: right'>${this.price} ${this.priceMaterial}</span></h1>
+          <hr >
           <p style='font-weight: bold'>${this.desc}</p>
           <p style='font-style: italic'>${this.filler}</p>
         </div>
@@ -82,6 +94,7 @@ Game.Launch = () => {
         this.owned++
         Game.rebuildInventory()
         Game.rebuildStore()
+        Game.drawZone()
       }
     }
 
@@ -108,6 +121,28 @@ Game.Launch = () => {
       <div id="store-spacer"></div>
     `
     s('#store-content').innerHTML = items
+  }
+
+  Game.drawZone = () => {
+    let el = s('#zone')
+    if (Game.items[0].owned == 1) {
+      el.style.visibility = 'visible'
+
+      //Get value of select tag
+      let value = el.options[el.selectedIndex].value
+
+      if (value == 'mine') {
+        console.log('you are currently in the mines')
+        s('#ore').style.display = 'initial'
+        s('#wood').style.display = 'none'
+      }
+
+      if (value == 'wood') {
+        console.log('you are currently in the woods')
+        s('#wood').style.display = 'initial'
+        s('#ore').style.display = 'none'
+      }
+    }
   }
 
   Game.rebuildStore()
