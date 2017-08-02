6e2dd58165a4c07049a8fda4670b72ae81ecf319
Synclair Wang <synclairwang11@yahoo.com>
2017-08-02 01:07:27 -0400

    rewriting code. I feel smart. Changed a 12 line function to 2 lines

---
+++ v0.2/main.js
@@ -0,0 +1,46 @@
+// Helper Functions
+let s = ((el) => {return document.querySelector(el)})
+
+let Game = {}
+
+Game.Launch = () => {
+  console.log('Game loaded and launched')
+
+  Game.ores = 0
+  Game.refinedOres = 0
+  Game.wood = 0
+
+  Game.oresPerClick = 1
+  Game.woodPerClick = 0
+
+  Game.earn = (amt, type) => {
+    Game[type] += amt
+  }
+
+  Game.spend = (amt, type) => {
+    Game[type] -= amt
+  }
+
+  Game.oreClick = () => {
+    Game.calculateClick()
+    Game.earn(Game.oresPerClick, 'ores')
+    Game.rebuildInventory()
+  }
+
+  s('#ore').onclick = Game.oreClick
+
+  Game.calculateClick = () => {
+    let amount = 0
+    amount += Game.oresPerClicks
+    // calculate the upgrdes and update Game.oresPerClick
+    Game.oresPerClicks = amount
+  }
+
+  Game.rebuildInventory = () => {
+    s('#inventory-ore').innerHTML = 'Ores: ' + Game.ores
+    s('#inventory-refined').innerHTML = 'Refined: ' + Game.refinedOres
+  }
+
+}
+
+window.onload = () => {Game.Launch()}
