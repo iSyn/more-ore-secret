a4b2fbcbc567309af8440837cd90fcfb81ab28ed
Synclair Wang <synclairwang11@yahoo.com>
2017-08-08 14:34:16 -0400

    got the hang of canvas

---
+++ v0.4/playground/playground.js
@@ -0,0 +1,20 @@
+
+let s = ((el) => {return document.querySelector(el)})
+
+let x = 0
+let y = 0
+
+s('#button').onclick = () => {
+  let canvas = s('#bottom-canvas')
+  let canvasContext = canvas.getContext("2d")
+  let image = s('#image')
+  canvasContext.drawImage(image, x, y)
+  x += 10
+  y = Math.floor(Math.random() * 20) + 1
+}
+
+
+// var c=document.getElementById("myCanvas");
+//     var ctx=c.getContext("2d");
+//     var img=document.getElementById("scream");
+//     ctx.drawImage(img,10,10);
