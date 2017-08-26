660932c9137bd7f402e54334c1398a50a830a60c
Synclair Wang <synclairwang11@yahoo.com>
2017-08-25 23:36:54 -0400

    working on ore hp

--- v0.5/main.js
+++ v0.5/main.js
@@ -4,6 +4,39 @@ let Game = {}
 
 Game.launch = () => {
   console.log('Game launched')
+
+  Game.ores = 0
+  Game.oreHp = 10
+  Game.orePerClick = 1
+  Game.level = {
+    currentLevel: 1,
+    currentStrength: 1,
+    currentLuck: 1,
+    currentXP: 0,
+    XPNeeded: 20,
+    availableSP: 0
+  }
+
+  Game.earn = (amount) => {
+    Game.ores += amount
+    Game.rebuildInventory()
+  }
+
+  Game.rebuildInventory = () => {
+    // s('.ores').innerHTML = 'Ores: ' + Game.ores.toFixed(1)
+    s('.ores').innerHTML = 'Ores: ' + Game.ores
+  }
+
+  Game.updatePercentage = () => {
+    let percentage = s('.ore-hp')
+
+  }
+
+  s('.ore').onclick = () => {
+    Game.earn(Game.orePerClick)
+  }
+
+
 }
 
 window.onload = () => {Game.launch()}
