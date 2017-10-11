f6f6882d44cf97c1487811f61bd7a34ee323811f
Synclair Wang <synclairwang11@yahoo.com>
2017-10-11 00:39:16 -0400

    working on quests

--- main.js
+++ main.js
@@ -2696,7 +2696,7 @@ Game.launch = () => {
               <div class="refined-store-item-bottom" style='text-align: center'>
                 <h2>${item.name}</h2>
                 <hr/>
-                <p style='color: white'>${item.desc}</p>
+                <p style='color: white; padding: 5px;'>${item.desc}</p>
                 <hr/>
                 <p style='color: white; margin-top: 20px;'>Cost: <i class='fa fa-diamond fa-1x' style="color:#00c0ff"></i> ${item.price}</p>
               </div>
@@ -2820,6 +2820,33 @@ Game.launch = () => {
     setTimeout(Game.mainLoop, 1000/Game.state.fps);
   }
 
+  Game.quests = []
+  let Quest = function(obj) {
+    this.name = obj.name
+    this.functionName = obj.name.replace(/ /g,'')
+    this.timesCompleted = 0
+    this.desc = obj.desc
+    this.locked = obj.locked
+    this.img = obj.img
+    this.completionTime = obj.completionTime
+    this.completionTimeTxt = obj.completionTimeTxt
+
+    Game.quests.push(this)
+  }
+
+  new Quest({
+    name: 'Abandoned Mineshaft',
+    locked: 0,
+    desc: 'Traverse into an abandoned mineshaft for hopes of greater rewards',
+    img: 'wip.png',
+    completionTime: 30,
+    completionTimeTxt: '30 minutes'
+  })
+  new Quest({name: 'Darkest Dungeon', locked: 1, desc: 'wip.png', img: 'wip.png'})
+  new Quest({name: 'Test name', locked: 2, desc: 'wip.png', img: 'wip.png'})
+  new Quest({name: 'Test name 2', locked: 2, desc: 'wip.png', img: 'wip.png'})
+  new Quest({name: 'Test name 3', locked: 2, desc: 'wip.png', img: 'wip.png'})
+
   Game.showQuests = () => {
     if (Game.state.player.lvl >= 10) {
       let div = document.createElement('div')
@@ -2829,21 +2856,36 @@ Game.launch = () => {
         <div class="quests-container">
           <h1 style='font-size: 3rem; padding: 10px 0;'>Quests</h1>
           <p onclick='document.querySelector(".wrapper").remove()' style='position: absolute; top: 5px; right: 5px; cursor: pointer'>X</p>
-          <hr/>
           <div class="active-quest-container">
+            <hr/>
             <p>No active quests :(</p>
+            <hr/>
           </div>
-          <hr/>
           <div class="available-quests-container">
-            <div class="available-quest">
-              <div class="available-quest-left">
-                <p>Abandoned Mineshaft</p>
-              </div>
-              <div class="available-quest-right">
-                <p>Go</p>
-              </div>
-            </div>
-            <div class="available-quest"></div>
+          `
+            for (i=0; i<Game.quests.length; i++) {
+              if (Game.quests[i].locked == 0) {
+                str += `
+                  <div class="available-quest unlocked" onclick="Game.showQuestInformation('${Game.quests[i].functionName}')">
+                    <p>${Game.quests[i].name}</p>
+                    <p class='quest-est-time' style='font-size: small'>completetion time: ${Game.quests[i].completionTimeTxt}</p>
+                  </div>
+                `
+              } else if (Game.quests[i].locked == 1) {
+                str += `
+                  <div style='opacity: .7' class="available-quest">
+                    <p>???</p>
+                  </div>
+                `
+              } else {
+                str += `
+                  <div style='opacity: .2' class="available-quest hidden-quest">
+                    <p>???</p>
+                  </div>
+                `
+              }
+            }
+          `
           </div>
         </div>
       `
@@ -2851,6 +2893,43 @@ Game.launch = () => {
       div.innerHTML = str
       s('body').append(div)
     }
+  }
+
+  Game.showQuestInformation = (questName) => {
+    let selectedQuest = {}
+    for (i=0; i<Game.quests.length; i++) {
+      if (Game.quests[i].functionName == questName) {
+        selectedQuest = Game.quests[i]
+      }
+    }
+
+    let div = document.createElement('div')
+    div.classList.add('wrapper')
+    div.innerHTML = `
+      <div class="quest-information">
+        <h1 style='padding:10px 0'>${selectedQuest.name}</h1>
+        <p style='position: absolute; top: 5px; right: 5px; cursor: pointer'>X</p>
+        <hr/>
+        <img src="./assets/${selectedQuest.img}" class="quest-img"'>
+        <hr/>
+        <p>${selectedQuest.desc} <br/> Completion Time: ${selectedQuest.completionTimeTxt}</p>
+        <hr/>
+        <div class="quest-information-bottom">
+          <div style='width: 40%' class="quest-information-bottom-left">
+            <p>TIMES COMPLETED: 0</p>
+            <p>QUEST REWARDS</p>
+            <p>5 completions - ???</p>
+            <p>10 completions - ???</p>
+            <p>20 completions - ???</p>
+          </div>
+          <div style='width: 60%' class="quest-information-bottom-right">
+            <button>Adventure <i class='fa fa-long-arrow-right fa-1x'></i></button>
+          </div>
+        </div>
+      </div>
+    `
+
+    s('body').append(div)
   }
 
   s('.bottom').addEventListener('mouseover', () => {
@@ -2865,6 +2944,14 @@ Game.launch = () => {
 
 
   Game.mainLoop()
+
+  s('#main-separator').addEventListener('click', () => {
+    Game.state.ores += 999999999999
+    Game.state.player.lvl += 10
+    Game.state.player.specializationSp += 10
+    Game.state.player.pickaxe.damage += 999999
+    Game.state.gems += 999
+  })
 
   // INIT SHIT
   buildInventory()
@@ -2948,7 +3035,11 @@ Game.launch = () => {
     if (isEscape) {
       if (s('.wrapper')) {
         let wrappers = document.querySelectorAll('.wrapper')
-        wrappers.forEach((wrapper) => wrapper.remove())
+        if (wrappers.length > 1) {
+          wrappers[1].remove()
+        } else {
+          wrappers.forEach((wrapper) => wrapper.remove())
+        }
       }
       if (s('.specialization-wrapper')) {
         s('.specialization-wrapper').remove()
