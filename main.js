01a1561e68db97aba1563f0cf11d932c2ab47f9c
Synclair Wang <synclairwang11@yahoo.com>
2018-05-02 01:01:36 -0400

    no! no libraries

--- main.js
+++ main.js
@@ -2334,52 +2334,6 @@ Game.launch = () => {
     let el = event.target
   }
 
-  interact('.inventory-item').draggable({
-    // enable inertial throwing
-    inertia: {
-      resistance: 30,
-      minSpeed: 300,
-      endSpeed: 300
-    },
-    // keep the element within the area of it's parent
-    restrict: {
-      restriction: "parent",
-      endOnly: true,
-      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-    },
-    // enable autoScroll
-    autoScroll: true,
-
-    // call this function on every dragmove event
-    onmove: dragMoveListener,
-    // call this function on every dragend event
-    onend: function (event) {
-      var textEl = event.target.querySelector('p');
-
-      textEl && (textEl.textContent =
-        'moved a distance of '
-        + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-                     Math.pow(event.pageY - event.y0, 2) | 0))
-            .toFixed(2) + 'px');
-    }
-  });
-
-  function dragMoveListener (event) {
-    var target = event.target,
-        // keep the dragged position in the data-x/data-y attributes
-        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-    // translate the element
-    target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
-
-    // update the posiion attributes
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-  }
-
   Game.toggleInventory = () => {
     let { inventorySlots, inventory } = Game.state.permanent
     let anchor = s('#left-separator').getBoundingClientRect()
