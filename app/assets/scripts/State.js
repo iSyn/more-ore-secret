b1486f78d67e0634571f8bc6ff9e12b4cab63754
Synclair Wang <synclairwang11@yahoo.com>
2018-11-02 17:12:48 -0400

    starting on game loop

---
+++ app/assets/scripts/State.js
@@ -0,0 +1,14 @@
+let State = function( state_obj = {} ) {
+
+  this.state = {
+    ores: state_obj.ores || 0,
+    gems: state_obj.ores || 0,
+    generation: 1,
+
+    stats: {
+      total_ores_earned: 0,
+      total_gems_earned: 0
+    }
+  }
+
+}
--- app/main.js
+++ app/main.js
@@ -1,3 +1,28 @@
-s('.hide-shop-btn').onclick = () => {
-  s('.right-container').classList.toggle('closed')
+let S = new State().state
+
+let Game = {}
+
+let hide_shop_btn = s('.hide-shop-btn')
+let right_container = s('.right-container')
+
+let update_topbar_inventory = () => {
+  let inventory_el = s('.topbar-inventory')
+  let str = `
+    <div class='left'>
+      <p>Ores: ${ S.ores }</p>
+      `
+      if ( S.stats.total_gems_earned > 0 ) {
+        str += `<p>Gems: ${ S.gems }</p>`
+      }
+
+      str += `
+    </div>
+    <div class='right'>
+      <p>Generation: ${ S.generation }</p>
+    </div>
+  `
+
+  inventory_el.innerHTML = str
 }
+
+hide_shop_btn.onclick = () => right_container.classList.toggle('closed')
