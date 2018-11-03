1d11732aceaf76ecd6ab1d9be953d4c96466b036
Synclair <synclairwang11@yahoo.com>
2018-11-02 23:30:32 -0400

    adding ore in

--- app/assets/scripts/State.js
+++ app/assets/scripts/State.js
@@ -1,9 +1,11 @@
-let State = function( state_obj = {} ) {
+let State = function( s = {} ) {
 
   this.state = {
-    ores: state_obj.ores || 0,
-    gems: state_obj.ores || 0,
-    generation: 1,
+    ores: s.ores || 0,
+    gems: s.ores || 0,
+    generation: s.generation || 1,
+    ops: s.ops || 0,
+    opc: s.opc || 1,
 
     stats: {
       total_ores_earned: 0,
